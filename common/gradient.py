759ce66a4704be96d34acc3406110a9593910d08
tsubauaaa <tsubasa1173@gmail.com>
2019-07-17 21:20:42 +0900

    ch07追加

--- common/gradient.py
+++ common/gradient.py
@@ -1,36 +1,34 @@
 # coding: utf-8
 import numpy as np
 
-
-def numerical_gradient_1d(f, x):
-    h = 1e-4
+def _numerical_gradient_1d(f, x):
+    h = 1e-4 # 0.0001
     grad = np.zeros_like(x)
-
-    # grad = x[idx]ごとの偏微分結果
+    
     for idx in range(x.size):
         tmp_val = x[idx]
-        # f(x+h)の計算
-        x[idx] = tmp_val + h
-        fxh1 = f(x)
-
-        # f(x-h)の計算
-        x[idx] = tmp_val - h
-        fxh2 = f(x)
-
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x) # f(x+h)
+        
+        x[idx] = tmp_val - h 
+        fxh2 = f(x) # f(x-h)
         grad[idx] = (fxh1 - fxh2) / (2*h)
-        x[idx] = tmp_val
-    
+        
+        x[idx] = tmp_val # 値を元に戻す
+        
     return grad
 
 
-def gradient_descent(f, init_x, lr=0.01, step_num=100):
-    x = init_x
-
-    for i in range(step_num):
-        grad = numerical_gradient(f, x)
-        x -= lr * grad
-    
-    return x
+def numerical_gradient_2d(f, X):
+    if X.ndim == 1:
+        return _numerical_gradient_1d(f, X)
+    else:
+        grad = np.zeros_like(X)
+        
+        for idx, x in enumerate(X):
+            grad[idx] = _numerical_gradient_1d(f, x)
+        
+        return grad
 
 
 def numerical_gradient(f, x):
