d808ed086b42fe9c9239d71b314157db7b6b3c73
tsubauaaa <tsubasa1173@gmail.com>
2019-07-28 09:44:50 +0900

    ch03更新

--- ch03/mnist_show.py
+++ ch03/mnist_show.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import sys, os
 import pathlib
 current_dir = pathlib.Path(__file__).resolve().parent
@@ -6,12 +7,13 @@ import numpy as np
 from dataset.mnist import load_mnist
 from PIL import Image
 
+
 def img_show(img):
     pil_img = Image.fromarray(np.uint8(img))
     pil_img.show()
 
-
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+
 img = x_train[0]
 label = t_train[0]
 print(label)
